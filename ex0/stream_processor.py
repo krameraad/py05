225a75e8983231f52f982be2778e5e8e8fe17eff
krameraad <eliaspowder@gmail.com>
2026-02-07 19:39:35 +0100

    ex0: formatting improvements

--- ex0/stream_processor.py
+++ ex0/stream_processor.py
@@ -3,6 +3,12 @@
 from typing import Any, List
 from abc import ABC, abstractmethod
 
+X = "\033[0m"
+R = "\033[91m"
+G = "\033[92m"
+H = "\033[1m"
+D = "\033[2m"
+
 
 class DataProcessor(ABC):
     @abstractmethod
@@ -19,7 +25,7 @@ class DataProcessor(ABC):
 
 class NumericProcessor(DataProcessor):
     def __init__(self):
-        print("Initializing Numeric Processor...")
+        print(f"{D}\nInitializing Numeric Processor...{X}")
 
     def process(self, data: Any) -> str:
         print("Processing data:", data)
@@ -27,7 +33,7 @@ class NumericProcessor(DataProcessor):
             amount = len(data)
             total = sum(data)
             return f"{amount} {total} {total / len(data)}"
-        return "Error!"
+        raise TypeError(f"input '{data}' is invalid for NumericProcessor")
 
     def validate(self, data: Any) -> bool:
         print("Validation: ", end="")
@@ -43,13 +49,13 @@ class NumericProcessor(DataProcessor):
 
     def format_output(self, result: str) -> str:
         args = result.split()
-        return f"Output: Processed {args[0]} numeric values: " \
+        return f"Processed {args[0]} numeric values: " \
                f"sum={args[1]}, avg={args[2]}"
 
 
 class TextProcessor(DataProcessor):
     def __init__(self):
-        print("Initializing Text Processor...")
+        print(f"{D}\nInitializing Text Processor...{X}")
 
     def process(self, data: Any) -> str:
         print("Processing data:", data)
@@ -57,7 +63,7 @@ class TextProcessor(DataProcessor):
             characters = len(data)
             words = len(data.split())
             return f"{characters} {words}"
-        return "Error!"
+        raise TypeError(f"input '{data}' is invalid for TextProcessor")
 
     def validate(self, data: Any) -> bool:
         print("Validation: ", end="")
@@ -69,19 +75,19 @@ class TextProcessor(DataProcessor):
 
     def format_output(self, result: str) -> str:
         args = result.split()
-        return f"Output: Processed text: " \
+        return f"Processed text: " \
                f"{args[0]} characters, {args[1]} words"
 
 
 class LogProcessor(DataProcessor):
     def __init__(self):
-        print("Initializing Log Processor...")
+        print(f"{D}\nInitializing Log Processor...{X}")
 
     def process(self, data: Any) -> str:
         print(f"Processing data: \"{data}\"")
         if self.validate(data):
             return data
-        return "Error!"
+        raise TypeError(f"input '{data}' is invalid for LogProcessor")
 
     def validate(self, data: Any) -> bool:
         print("Validation: ", end="")
@@ -99,21 +105,30 @@ class LogProcessor(DataProcessor):
         level = strs[0].removesuffix(":").upper()
         del strs[0]
         result = " ".join(strs)
-        return f"Output: [{level}] {level} level detected: {result}"
+        return f"[{level}] {level} level detected: {result}"
 
 
 if __name__ == "__main__":
     def test_processor(proc: DataProcessor, data: Any) -> None:
-        out = proc.process(data)
-        print(proc.format_output(out))
+        try:
+            out = proc.process(data)
+            print(f"Output: {proc.format_output(out)}")
+        except TypeError as e:
+            print(f"{R}Error: {e}{X}")
 
-    print("=== CODE NEXUS - DATA PROCESSOR FOUNDATION ===")
-    test_processor(NumericProcessor(), [1, 2, 3, 4, 5])
-    test_processor(TextProcessor(), "Hello Nexus World")
-    test_processor(LogProcessor(), "ERROR: Connection timeout")
-    print("=== Polymorphic Processing Demo ===")
-    print("Processing multiple data types through same interface...")
-    test_processor(NumericProcessor(), [1, 2, 3])
-    test_processor(TextProcessor(), "Hey guys")
-    test_processor(LogProcessor(), "INFO: System ready")
-    print("Foundation systems online. Nexus ready for advanced streams.")
+    print(f"{H}\n=== CODE NEXUS - DATA PROCESSOR FOUNDATION ==={X}")
+    num = NumericProcessor()
+    test_processor(num, [1, 2, 3, 4, 5])
+    txt = TextProcessor()
+    test_processor(txt, "Hello Nexus World")
+    log = LogProcessor()
+    test_processor(log, "ERROR: Connection timeout")
+    print(f"{H}\n=== Polymorphic Processing Demo ==={X}")
+    print(f"{D}Processing multiple data types through same interface...{X}")
+    test_processor(num, [1, 2, 3])
+    print()
+    test_processor(txt, "Hey guys")
+    print()
+    test_processor(log, "INFO: System ready")
+    print(f"{G}\nFoundation systems online."
+          f" Nexus ready for advanced streams.{X}")
